9bca7d207ae6573464c119b6f094c1bb9c2e604f
James Blair <jblair@feature23.com>
2025-02-23 13:37:41 -0700

    Add Testcontainers.PostgreSql for integration testing with Docker

--- Squill.IntegrationTests/Postgres/PostgresIntegrationTestBase.cs
+++ Squill.IntegrationTests/Postgres/PostgresIntegrationTestBase.cs
@@ -1,6 +1,15 @@
+using Testcontainers.PostgreSql;
+
 namespace Squill.IntegrationTests.Postgres;
 
-public abstract class PostgresIntegrationTestBase
+public abstract class PostgresIntegrationTestBase : IAsyncLifetime
 {
-    
+    private readonly PostgreSqlContainer _postgresqlContainer = new PostgreSqlBuilder()
+        .Build();
+
+    public string ConnectionString => _postgresqlContainer.GetConnectionString();
+
+    public Task InitializeAsync() => _postgresqlContainer.StartAsync();
+
+    public Task DisposeAsync() => _postgresqlContainer.DisposeAsync().AsTask();
 }
